408c0639640c6ab1cb742fdc5eb19651a6745f3b
warrior <warriorfoox@gmail.com>
2020-12-07 09:54:59 -0500

    Add RemoveScree and AddScreen

--- src/replicatedstorage/Modules/Interface/init.lua
+++ src/replicatedstorage/Modules/Interface/init.lua
@@ -1,5 +1,5 @@
 -- ++ 2.12.2020 [Create Interface]
--- // 3.12.2020 [Create Notification]
+-- // 7.12.2020 [Add RemoveScreen and AddScreen]
 -- Handles GUI based things.
 
 -- -- Documentation
@@ -9,9 +9,16 @@
 -- >> =>  Description: Binds a button (and function) to a prefab.
 -- >> +>         Arg1: Button = GuiButton
 -- >> +>         Arg2: OnClick = Function
--- >> ++ CreateNotification(): Function
+-- >> ++ CreateNotification()
 -- >> =>          Description: Creates a Notification.
 -- >> +>                 Arg1: Data = table pairs{string = number | string}
+-- >> ++ AddScreen(): Instance
+-- >> => Description: Directly enables or clones a GUI.
+-- >> +>        Arg1: Object = Instance
+-- >> ++ RemoveScreen(): boolean
+-- >> =>    Description: Directly disables or destroys a GUI.
+-- >> +>           Arg1: Object = string | Instance
+-- >> ?>           Arg2: Disable = boolean
 
 local Depends = require(game:GetService("ReplicatedStorage"):WaitForChild("Depends"))
 local LModules = Depends.Require(script, {"ButtonFunctions", "Notification"})
@@ -25,7 +32,42 @@ function Interface:BindButton(Button, OnClick)
 end
 
 function Interface:CreateNotification(Type, Text)
+    assert(type(Type) == "string" and type(Text) == "string")
     LModules.Notification:CreateNotification(Type, Text)
+end
+
+function Interface:AddScreen(Object)
+    assert(typeof(Object) == "Instance")
+
+    if Object.Parent == Depends.PlayerGui then
+        Object.Enabled = true
+    else
+        Object = Object:Clone()
+        Object.Parent = Depends.PlayerGui
+        Object.Enabled = true
+    end
+
+    return Object
+end
+
+function Interface:RemoveScreen(Object, Disable)
+    assert(type(Object) == "string" or typeof(Object) == "Instance")
+
+    if type(Object) == "string" then
+        Object = Depends.PlayerGui:FindFirstChild(Object)
+    end
+
+    if Object then
+        if Disable then
+            Object.Enabled = false
+        else
+            Object:Destroy()
+        end
+
+        return true
+    end
+
+    return false
 end
 
 return Interface
