2e43756fb2e5421e2ad4bc57599d56c2cb128f72
warrior <31458482+warfurret@users.noreply.github.com>
2021-06-30 19:03:13 -0400

    Update Tween.lua

--- src/replicatedstorage/Modules/Tween.lua
+++ src/replicatedstorage/Modules/Tween.lua
@@ -1,38 +1,33 @@
--- 25.05.2021
+local TweenService = game:GetService('TweenService');
 
--- Services
-local TweenService = game:GetService("TweenService")
-
--- Main Function
 local function TweenNextObject(Object, Info, Dictionary, AutoPlay, DefaultValues)
-    local TweenObject = TweenService:Create(Object, Info, Dictionary)
+	local TweenObject = TweenService:Create(Object, Info, Dictionary);
 
-    if DefaultValues then
-        for Property, Value in pairs(DefaultValues) do
-            Object[Property] = Value
-        end
-    end
+	if DefaultValues then
+		for Property, Value in pairs(DefaultValues) do
+			Object[Property] = Value;
+		end;
+	end;
 
-    if AutoPlay then
-        TweenObject:Play()
-    end
+	if AutoPlay then
+		TweenObject:Play();
+	end;
 
-    return TweenObject
-end
+	return TweenObject;
+end;
 
--- Main Module
 return {
-    new = function(Object, Info, Dictionary, AutoPlay, DefaultValues)
-        if type(Object) == "table" then
-            local ReturnTable = {}
+	new = function(Object, Info, Dictionary, AutoPlay, DefaultValues)
+		if type(Object) == 'table' then
+			local ReturnTable = {};
 
-            for _, NextObject in ipairs(Object) do
-                table.insert(ReturnTable, TweenNextObject(NextObject, Info, Dictionary, AutoPlay, DefaultValues))
-            end
+			for _, NextObject in ipairs(Object) do
+				table.insert(ReturnTable, TweenNextObject(NextObject, Info, Dictionary, AutoPlay, DefaultValues));
+			end;
 
-            return ReturnTable
-        else
-            return TweenNextObject(Object, Info, Dictionary, AutoPlay, DefaultValues)
-        end
-    end
-}
+			return ReturnTable;
+		else
+			return TweenNextObject(Object, Info, Dictionary, AutoPlay, DefaultValues);
+		end;
+	end
+};
