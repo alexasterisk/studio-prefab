1da70bd135b20d7c3febec17d5c4a34e5dd5bb41
warrior <31458482+warfurret@users.noreply.github.com>
2021-07-01 19:50:52 -0400

    fix

--- src/replicatedstorage/Modules/AutoCanvasSize.lua
+++ src/replicatedstorage/Modules/AutoCanvasSize.lua
@@ -1,71 +1,13 @@
 local Module = {};
 local Connections = {};
 
-local function GetHorizontalAmount(Table)
-	local Current = -1;
-	local Absolutes = {};
-	
-	for _, Object in pairs(Table) do
-		local AbsoluteY = Object.AbsolutePosition.Y;
-		
-		if Absolutes[AbsoluteY] then
-			table.insert(Absolutes[AbsoluteY], Object);
-		else
-			Absolutes[AbsoluteY] = {Object};
-		end;
-	end;
-	
-	for _, Objects in pairs(Absolutes) do
-		if #Objects > Current then
-			Current = #Objects;
-		end;
-	end;
-	
-	return Current;
-end;
-
-local function GetVerticalAmount(Table)
-	local Current = -1;
-	local Absolutes = {};
-	
-	for _, Object in pairs(Table) do
-		local AbsoluteX = Object.AbsolutePosition.X;
-		
-		if Absolutes[AbsoluteX] then
-			table.insert(Absolutes[AbsoluteX], Object);
-		else
-			Absolutes[AbsoluteX] = {Object};
-		end;
-	end;
-	
-	for _, Objects in pairs(Absolutes) do
-		if #Objects > Current then
-			Current = #Objects;
-		end;
-	end;
-	
-	return Current;
-end;
-
 function Module:Connect(Scroller, IsOnlyY)
 	local Layout = Scroller:FindFirstChildWhichIsA('UIListLayout') or Scroller:FindFirstChildWhichIsA('UIGridLayout');
-	
-	local function GetObjects()
-		local Objects = {};
-		
-		for _, Object in ipairs(Scroller:GetChildren()) do
-			if Object:IsA('GuiObject') then
-				table.insert(Objects, Object);
-			end;
-		end;
-		
-		return Objects;
-	end
-	
+
 	local function Update()
 		Scroller.CanvasSize = UDim2.new(0, IsOnlyY and 0 or Layout.AbsoluteContentSize.X, 0, Layout.AbsoluteContentSize.Y + 25);
 	end;
-	
+
 	Connections[Scroller] = {
 		Layout:GetPropertyChangedSignal('AbsoluteContentSize'):Connect(Update),
 
@@ -74,14 +16,14 @@ function Module:Connect(Scroller, IsOnlyY)
 				self:Disconnect(Scroller);
 			end;
 		end),
-		
+
 		Scroller.ChildAdded:Connect(function()
 			spawn(Update);
 		end),
-		
+
 		Scroller.ChildRemoved:Connect(Update)
 	};
-	
+
 	Update();
 end;
 
@@ -92,7 +34,7 @@ function Module:Disconnect(Scroller)
 				Connection:Disconnect();
 			end;
 		end;
-		
+
 		Connections[Scroller] = nil;
 	end;
 end;
