e11b8b54fd735d4e38262de0024f6ce7d5a5e754
Warfurret <31458482+warfurret@users.noreply.github.com>
2021-07-15 19:02:28 -0400

    Remove unused functions

--- src/replicatedstorage/Modules/Objecat.lua
+++ src/replicatedstorage/Modules/Objecat.lua
@@ -1,9 +1,3 @@
--- extends:
--- Instance.new
--- Instance:FindFirstChild
--- Instance:GetAttribute
--- Instance:SetAttribute
-
 local Module = {};
 
 function Module:Create(Type, Properties, Children)
@@ -48,24 +42,6 @@ function Module:Clone(Object, Properties)
 	return Cloned;
 end;
 
-function Module:Weld(Model, MainPart)
-	assert(typeof(Model) == 'Instance');
-
-	if typeof(MainPart == 'Instance') then
-		for _, Child in ipairs(Model:GetChildren()) do
-			if Child ~= MainPart and Child:IsA('BasePart') then
-				self:Create('WeldConstraint', {
-					Parent = MainPart,
-					Part0 = MainPart,
-					Part1 = Child
-				});
-			end;
-		end;
-	end;
-
-	return Model;
-end;
-
 function Module:FindOrMake(Search, Type, Name)
 
 	local WasCreated = false;
@@ -101,37 +77,6 @@ function Module:FindOrMake(Search, Type, Name)
 	end;
 
 	return Object, WasCreated;
-end;
-
--- this does remove the ability of functions lol
-function Module:AttributesToVariables(Object)
-	local Metatable = {
-		__index = function(_, Value)
-			assert(type(Value) == 'string');
-
-			if Object[Value] ~= nil then
-				return Object[Value];
-			else
-				if Object:GetAttribute(Value) ~= nil then
-					return Object:GetAttribute(Value);
-				end;
-			end;
-		end,
-
-		__newindex = function(Table, Index, Value)
-			assert(type(Index) == 'string');
-
-			if Object[Index] ~= nil then
-				Object[Index] = Value;
-			else
-				Object:SetAttribute(Index, Value);
-			end;
-
-			return Table[Index];
-		end;
-	};
-
-	return setmetatable({}, Object);
 end;
 
 return Module;
