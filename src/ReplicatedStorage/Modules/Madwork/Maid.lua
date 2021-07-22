33a3997eef4dd07b39162501cab9a9266a802b85
Warfurret <31458482+warfurret@users.noreply.github.com>
2021-07-22 00:49:25 -0400

    Remake Maid

--- src/ReplicatedStorage/Modules/Madwork/Maid.lua
+++ src/ReplicatedStorage/Modules/Madwork/Maid.lua
@@ -1,109 +1,73 @@
---[[
-{Madwork}
--[MadworkMaid]---------------------------------------
-	Madwork implementation of Maid.lua
-	
-	Functions:
-	
-		MadworkMaid.NewMaid() --> [Maid]
-		MadworkMaid.Cleanup(task, params...)
-	
-	Members [Maid]:
-	
-		Maid:AddCleanupTask(task)
-		Maid:RemoveCleanupTask(task)
-		Maid:Cleanup(params...)
-		
-	Notice: "params..." argument is optional and can be used to pass arguments to cleanup functions
-	
---]]
+local function PerformCleanupTask(Task, ...)
+	if type(Task) == 'function' then
+		Task(...);
+	elseif typeof(Task) == 'RBXScriptSignal' then
+		Task:Disconnect();
+	elseif typeof(Task) == 'Instance' then
+		Task:Destroy();
+	elseif type(Task) == 'table' then
+		if type(Task.Destroy) == 'function' then
+			Task:Destroy();
+		elseif type(Task.Disconnect) == 'function' then
+			Task:Disconnect();
+		end;
+	end;
+end;
 
------ Module Table -----
+local Module = {};
+Module._CleanupTasks = {};
+Module._IsCleaned = {};
+Module.__index = Module;
 
-local MadworkMaid = {
-	
-}
+function Module:Add(Task)
+	if self._IsCleaned == true then
+		PerformCleanupTask(Task);
+	end;
 
------ Private functions -----
-
-local function PerformCleanupTask(task, ...)
-	if type(task) == "function" then
-		task(...)
-	elseif typeof(task) == "RBXScriptConnection" then
-		task:Disconnect()
-	elseif typeof(task) == "Instance" then
-		task:Destroy()
-	elseif type(task) == "table" then
-		if type(task.Destroy) == "function" then
-			task:Destroy()
-		elseif type(task.Disconnect) == "function" then
-			task:Disconnect()
-		end
-	end
-end
-
------ Public functions -----
-
--- Maid object:
-
-local Maid = {
-	-- _cleanup_tasks = {},
-	-- _is_cleaned = false,
-}
-Maid.__index = Maid
-
-function Maid:AddCleanupTask(task)
-	if self._is_cleaned == true then
-		PerformCleanupTask(task)
-	end
-	if type(task) == "function" then
-		table.insert(self._cleanup_tasks, task)
-	elseif typeof(task) == "RBXScriptConnection" then
-		table.insert(self._cleanup_tasks, task)
-	elseif typeof(task) == "Instance" then
-		table.insert(self._cleanup_tasks, task)
-	elseif type(task) == "table" then
-		if type(task.Destroy) == "function" then
-			table.insert(self._cleanup_tasks, task)
-		elseif type(task.Disconnect) == "function" then
-			table.insert(self._cleanup_tasks, task)
+	if type(Task) == 'function' or typeof(Task) == 'RBXScriptSignal' or typeof(Task) == 'Instance' then
+		table.insert(self._CleanupTasks, Task);
+	elseif type(Task) == 'table' then
+		if type(Task.Destroy) == 'function' or type(Task.Disconnect) == 'function' then
+			table.insert(self._CleanupTasks, Task);
 		else
-			error("[MadworkMaid]: Received object table as cleanup task, but couldn't detect a :Destroy() method")
-		end
+			error('[MAID]: Received object table as cleanup task, but couldn\'t detect a :Destroy() method!');
+		end;
 	else
-		error("[MadworkMaid]: Cleanup task of type \"" .. typeof(task) .. "\" not supported")
-	end
-end
+		error(string.format('[MAID]: Cleanup task of type "%s" is not supported!', typeof(Task)));
+	end;
+end;
 
-function Maid:RemoveCleanupTask(task)
-	local cleanup_tasks = self._cleanup_tasks
-	for i = 1, #cleanup_tasks do
-		if cleanup_tasks[i] == task then
-			table.remove(cleanup_tasks, i)
-			break
-		end
-	end
-end
+function Module:Remove(Task)
+	local CleanupTasks = self._CleanupTasks;
+	Task = table.find(CleanupTasks, Task);
 
-function Maid:Cleanup(...)
-	for _, task in ipairs(self._cleanup_tasks) do
-		PerformCleanupTask(task, ...)
-	end
-	self._cleanup_tasks = {}
-	self._is_cleaned = true
-end
+	if Task then
+		table.remove(CleanupTasks, Task);
+	end;
+end;
 
--- New maid object:
+function Module:Clean(...)
+	for _, Task in ipairs(self._CleanupTasks) do
+		PerformCleanupTask(Task, ...);
+	end;
 
-function MadworkMaid.NewMaid() --> [Maid]
-	local maid = {
-		_cleanup_tasks = {},
-		_is_cleaned = false,
-	}
-	setmetatable(maid, Maid)
-	return maid
-end
+	self._CleanupTasks = {};
+	self._IsCleaned = true;
+end;
 
-MadworkMaid.Cleanup = PerformCleanupTask
+local Maid = {};
 
-return MadworkMaid
+function Maid.new()
+	local maid = {};
+	maid._CleanupTasks = {};
+	maid._IsCleaned = false;
+
+	setmetatable(maid, Module);
+	return maid;
+end;
+
+function Maid:CleanAll(...)
+	PerformCleanupTask(...);
+end;
+
+return Maid;
